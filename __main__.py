b63d9b76d1be99bd8d5ae54543c11bfda5924d3e
Mathias Mellingen <mellingenmathias@gmail.com>
2024-01-26 15:25:29 +0100

    Added vuln1 and modules files

--- __main__.py
+++ __main__.py
@@ -1,15 +1,7 @@
 """A Python Pulumi program"""
-import pulumi_azure as azure
-import add_users
-import container
-from create_azure_devops import CreateAzureDevops
 
-resource_group = azure.core.ResourceGroup('resource-group', location="West Europe")
 
-github_repo_url = "https://github.com/Oslolosen/bachelor_docs.git"
+from vulnerabilities.vuln1 import main as vuln1
 
-azure_devops = CreateAzureDevops("testproject", "This is a test project.", "bachelor2024", resource_group)
-azure_devops.import_github_repo(github_repo_url, "testrepo")
-#azure_devops.create_work_item(2)
-azure_devops.add_flag_pipeline_secret("flag1", "FLAG{testflag}")
-azure_devops.create_ci_cd_pipeline("testpipeline")
+
+vuln1.start()
---
+++ vulnerabilities/vuln1/main.py
@@ -0,0 +1,17 @@
+import pulumi_azure as azure
+from source.add_users import CreateUsers
+from source.create_azure_devops import CreateAzureDevops
+from source.container import CreateContainer
+
+
+
+def start():
+    resource_group = azure.core.ResourceGroup('resource-group', location="West Europe")
+
+    github_repo_url = "https://github.com/Oslolosen/bachelor_docs.git"
+
+    azure_devops = CreateAzureDevops("testproject", "This is a test project.", "bachelor2024", resource_group)
+    azure_devops.import_github_repo(github_repo_url, "testrepo")
+    #azure_devops.create_work_item(2)
+    azure_devops.add_flag_pipeline_secret("flag1", "FLAG{testflag}")
+    azure_devops.create_ci_cd_pipeline("testpipeline")
