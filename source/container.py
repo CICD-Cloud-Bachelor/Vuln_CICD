3a8f7b4a2a08134f1f83a02add71342d5f528b0f
Mathias Mellingen <mellingenmathias@gmail.com>
2024-01-30 12:34:01 +0100

    Updated DockerACR class to start container with public hostname

--- source/container.py
+++ source/container.py
@@ -2,6 +2,8 @@ import pulumi
 import pulumi_azure as azure
 import pulumi_docker as docker
 import pulumi_azure_native as azure_native
+from pulumi_azure_native import containerinstance, resources
+from pulumi_azure_native import network, dbformysql
 
 class CreateContainer:    
     def __init__(self, resource_group, name: str, image: str, memory: float, port: int, cpu: int):
@@ -45,21 +47,19 @@ class CreateContainer:
 
 
 
-class ImportDockerImageToACR:
-    REGISTRY_NAME = "myregistry" 
+class DockerACR:
     IMAGE_TAG = "v1.0"
-    def __init__(self, resource_group, image_name) -> None:
+    DNS_LABEL = "pulumibachelorproject"
+    def __init__(self, resource_group, registry_name) -> None:
         self.resource_group = resource_group
-        self.image_name = image_name
-        self.image_context_path = f"source/docker_images/{self.image_name}"
-        self.dockerfile_path = f"{self.image_context_path}/Dockerfile" 
+        self.registry_name = registry_name
         self.__create_registry()
 
 
     def __create_registry(self) -> None:
-        pulumi.log.debug(f"Creating registry: {self.REGISTRY_NAME}")
+        pulumi.log.info(f"Creating registry: {self.registry_name}")
         self.registry = azure_native.containerregistry.Registry(
-            self.REGISTRY_NAME,
+            self.registry_name,
             resource_group_name=self.resource_group.name,
             sku=azure_native.containerregistry.SkuArgs(
                 name="Basic"
@@ -67,9 +67,6 @@ class ImportDockerImageToACR:
             location=self.resource_group.location,
             admin_user_enabled=True,
         )
-
-        self.image_name_with_tag = pulumi.Output.all(self.registry.login_server, self.image_name, self.IMAGE_TAG).apply(lambda args: f"{args[0].lower()}/{args[1]}:{args[2]}")
-
 
         # Obtain registry credentials for Docker
         self.credentials = pulumi.Output.all(self.resource_group.name, self.registry.name).apply(
@@ -79,14 +76,20 @@ class ImportDockerImageToACR:
             )
         )
 
-    def build_and_push_docker(self) -> str:
+    def build_and_push_docker_image(self, image_name: str) -> str:
+        pulumi.log.info(f"Building and pushing docker image: {image_name}")
+        
+        image_context_path = f"source/docker_images/{image_name}"
+        dockerfile_path = f"{image_context_path}/Dockerfile" 
+        image_name_with_tag = pulumi.Output.all(self.registry.login_server, image_name, self.IMAGE_TAG).apply(lambda args: f"{args[0].lower()}/{args[1]}:{args[2]}")
+        
         # Define a Docker image resource that builds and pushes the image
         image = docker.Image(
             "myimage",
-            image_name=self.image_name_with_tag,
+            image_name=image_name_with_tag,
             build=docker.DockerBuildArgs(
-                context=self.image_context_path,
-                dockerfile=self.dockerfile_path,
+                context=image_context_path,
+                dockerfile=dockerfile_path,
                 platform="linux/amd64",
             ),
             registry=docker.RegistryArgs(
@@ -99,3 +102,53 @@ class ImportDockerImageToACR:
 
         pulumi.export("image_name", image.image_name.apply(lambda name: name))
         return image.image_name.apply(lambda name: name)
+
+    def start_container(self, image_name: str, container_name: str, port: int, cpu: float, memory: float):
+        pulumi.log.info(f"Starting container: {container_name}")
+        
+        container_group_name = container_name + "-group"
+        
+        # Create a container group with a single container
+        container_group = containerinstance.ContainerGroup(
+            container_group_name,
+            resource_group_name=self.resource_group.name,
+            os_type="Linux",  # or "Windows"
+            containers=[
+                containerinstance.ContainerArgs(
+                    name=container_name,
+                    image=image_name,
+                    resources=containerinstance.ResourceRequirementsArgs(
+                        requests=containerinstance.ResourceRequestsArgs(
+                            cpu=cpu,
+                            memory_in_gb=memory,
+                        ),
+                    ),
+                    ports=[containerinstance.ContainerPortArgs(port=port)],
+                ),
+            ],
+            image_registry_credentials=[
+                containerinstance.ImageRegistryCredentialArgs(
+                    server=self.registry.login_server.apply(lambda server: server),
+                    username=self.credentials.apply(lambda creds: creds.username),
+                    password=self.credentials.apply(lambda creds: creds.passwords[0].value),
+                ),
+            ],
+            ip_address=containerinstance.IpAddressArgs(
+                ports=[containerinstance.PortArgs(
+                    protocol="TCP",
+                    port=port,
+                )],
+                type="Public",
+                dns_name_label=f"{container_name}{self.DNS_LABEL}", # optional
+            ),
+            restart_policy="OnFailure",
+        )
+
+        # Construct the FQDN
+        fqdn = pulumi.Output.all(container_group.name, container_group.location).apply(
+            lambda args: f"{container_name}{self.DNS_LABEL}.{args[1]}.azurecontainer.io"
+        )
+
+        # Export the FQDN
+        pulumi.export('fqdn', fqdn)
+        pulumi.export("container_group_id", container_group.id)
--- vulnerabilities/vuln4/main.py
+++ vulnerabilities/vuln4/main.py
@@ -1,12 +1,12 @@
 import pulumi_azure as azure
 from source.add_users import CreateUsers
 from source.create_azure_devops import CreateAzureDevops
-from source.container import CreateContainer, ImportDockerImageToACR
+from source.container import CreateContainer, DockerACR
 
 
 
 def start(resource_group: azure.core.ResourceGroup):
-    acr = ImportDockerImageToACR(resource_group, "mysqldb")
-    string = acr.build_and_push_docker()
+    acr = DockerACR(resource_group, "registrypulumi")
+    acr_string = acr.build_and_push_docker_image("mysqldb")
 
-    
+    acr.start_container(acr_string, "mysql-container", 3306, 1.0, 1.0)
