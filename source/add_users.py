15e989da7e5fa4c9de1119f1d836c53f0ba93ead
Mo <mo@mothpad.mo>
2024-02-05 14:23:55 +0100

    vscode test commit

--- source/add_users.py
+++ source/add_users.py
@@ -6,32 +6,21 @@ from pulumi_azuread import User
 
 class CreateUsers:
     fake = Faker()
-    domain = "bacheloroppgave2024proton.onmicrosoft.com"
-    user_details = []
     
-    def __init__(self, amount: int) -> None:
-        self.amount = amount
-        self.create_users()
-        self.create_users_in_azure_ad()
+    def __init__(self, domain: str) -> None:
+        self.domain = domain
 
-    def create_users(self) -> None:
-        pulumi.log.debug(f"Creating {self.amount} users")
-        for _ in range(self.amount):
-            name = self.fake.name()
-            pulumi.log.debug(f"Creating user: {name}")
-            self.user_details.append({"display_name": name, "user_principal_name": f"{name.replace(' ', '.')}@{self.domain}", "password": self.fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)})
+    def create_user(self, name: str, password: str) -> User:
+        pulumi.log.info(f"Creating user in Azure AD: {name}")
+        self.user_object = User(f"{name}",
+                                    display_name = name,
+                                    user_principal_name = f"{name.replace(' ', '.')}@{self.domain}",
+                                    password = password,
+                                    force_password_change = False # Set to True if you want to force a password change on first login
+                                    )
+        pulumi.export(f"{name}_object_id", self.user_object.id)
+        return self.user_object
 
-    def create_users_in_azure_ad(self):
-        pulumi.log.debug(f"Creating users in Azure AD")
-        for index, user in enumerate(self.user_details):
-            user_object = User(f"user{index}",
-                            display_name=user["display_name"],
-                            user_principal_name=user["user_principal_name"],
-                            password=user["password"],
-                            force_password_change=False # Set to True if you want to force a password change on first login
-                            )
-            
-            pulumi.log.debug(f"Created user: {user_object.display_name}")
-            pulumi.export(f"user{index}_object_id", user_object.id)
-
+    def __randomPass(self) -> str:
+        return self.fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
 
--- vulnerabilities/vuln3/main.py
+++ vulnerabilities/vuln3/main.py
@@ -1,4 +1,7 @@
+import pulumi
 import pulumi_azure as azure
+import pulumi_azuread as azuread
+import pulumi_azuredevops as azuredevops
 from source.add_users import CreateUsers
 from source.create_azure_devops import CreateAzureDevops
 from source.container import CreateContainer
@@ -10,5 +13,29 @@ def start():
 
     github_repo_url = "https://github.com/flis5/svakhet3.git"
 
-    azure_devops = CreateAzureDevops("Vulnerability 3", "Insufficient Credential Hygiene", "bachelor2024", resource_group)
-    azure_devops.import_github_repo(github_repo_url, "Vulnerabity_3")
+    adoEnv = CreateAzureDevops("Vulnerability 3", "Insufficient Credential Hygiene", "hackegutta", resource_group)
+    adoEnv.import_github_repo(github_repo_url, "Vulnerabity_3")
+
+    userCreator = CreateUsers("mohammedhussaini1268gmail.onmicrosoft.com")
+    ad_user = userCreator.create_user("Troll_Trollington", "Troll57Hoho69%MerryChristmas")
+
+    devops_user = azuredevops.User("Troll",
+                                   principal_name = ad_user.user_principal_name
+                                   )
+    
+
+    # Create an Azure DevOps Group at the project level
+    devops_group = azuredevops.Group("devOpsGroup",
+        scope = adoEnv.project.id, # Scope the group to the project level
+        display_name="PulumiManagedDevOpsGroup",
+        members = [
+            devops_user.descriptor
+        ]
+        )
+
+
+    # Export the IDs of the created groups
+    pulumi.export('azureDevOpsGroupIdExport', devops_group.id)
+
+
+    
