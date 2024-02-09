18dcf16038d9c8b879b7ce1ee0c221f8d1ee0eb2
Mathias Mellingen <mellingenmathias@gmail.com>
2024-02-09 14:06:04 +0100

    Added workitem.py file to create workitems
    The WorkItem class accepts type, title, description and comment for each workitem

--- __main__.py
+++ __main__.py
@@ -1,11 +1,12 @@
 import pulumi_azure as azure
-
-from vulnerabilities.vuln1 import main as vuln1
+from pulumi import Config
+#from vulnerabilities.vuln1 import main as vuln1
 from vulnerabilities.vuln4 import main as vuln4
-from vulnerabilities.vuln5 import main as vuln5
+#from vulnerabilities.vuln5 import main as vuln5
 
-
-resource_group = azure.core.ResourceGroup('resource-group', location="West Europe")
+config = Config("azure-native")
+location = config.get("location")
+resource_group = azure.core.ResourceGroup('resource-group', location=location)
 
 
 
--- vulnerabilities/vuln4/main.py
+++ vulnerabilities/vuln4/main.py
@@ -1,20 +1,21 @@
 import pulumi_azure as azure
-from source.add_users import CreateUsers
 from source.create_azure_devops import CreateAzureDevops
 from source.container import DockerACR
-
-
+from source.workitem import WorkItem
 
 def start(resource_group: azure.core.ResourceGroup):
-    CreateUsers(1, "Test User", "TestPassword123")
     # acr = DockerACR(resource_group, "registrypulumi")
     # acr_string = acr.build_and_push_docker_image("mysqldb")
     # connection_string = acr.start_container(acr_string, "mysql-container", [3306], 1.0, 1.0)
     connection_string = "mysql://root:xxxxxxxxxxxxxx@xxxxxxxxxxxxxxx:3306/prod"
     github_repo_url = "https://github.com/CICD-Cloud-Bachelor/VULN4.git"
     azure_devops = CreateAzureDevops("VULN4", "Project for VULN4", "bachelor2024", resource_group)
-    azure_devops.import_github_repo(github_repo_url, "VULN4_REPO")
-    #azure_devops.create_work_item(40)
-    azure_devops.add_variables({"CONNECTION_STRING": connection_string, "DATABASE": "prod", "USERNAME": "root", "PASSWORD": "myr00tp455w0rd"})
-    azure_devops.create_ci_cd_pipeline("testpipeline")
+    #azure_devops.import_github_repo(github_repo_url, "VULN4_REPO")
+    #azure_devops.create_work_item(5)
+    #azure_devops.add_variables({"CONNECTION_STRING": connection_string, "DATABASE": "prod", "USERNAME": "root", "PASSWORD": "myr00tp455w0rd"})
+    #azure_devops.create_ci_cd_pipeline("testpipeline")
     #azure_devops.run_pipeline("main")
+    workitem = WorkItem("VULN4", azure_devops.project)
+    #workitem.create("Task", "Magnus Magnusen", "yoooooooooooooooooooooooo", "kommentarhei")
+    #workitem.create("Epic", "Mo Moesen", "heihei", "kommentarsandkasndkajs")
+    workitem.create_random_work_items(1)
