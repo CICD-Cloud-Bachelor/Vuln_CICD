575788b3ed7c3affba0e17103d2e844b963393c9
Oslolosen <97946230+Oslolosen@users.noreply.github.com>
2024-02-12 13:25:59 +0100

    Basic edit
    Fix av conflict

--- __main__.py
+++ __main__.py
@@ -1,14 +1,11 @@
 import pulumi_azure as azure
-<<<<<<< HEAD
 
 from vulnerabilities.vuln1 import main as vuln1
 from vulnerabilities.vuln2 import main as vuln2
-=======
-from pulumi import Config
-#from vulnerabilities.vuln1 import main as vuln1
->>>>>>> Dev
 from vulnerabilities.vuln4 import main as vuln4
 #from vulnerabilities.vuln5 import main as vuln5
+
+from pulumi import Config
 
 config = Config("azure-native")
 location = config.get("location")
@@ -17,12 +14,6 @@ resource_group = azure.core.ResourceGroup('resource-group', location=location)
 
 
 #vuln1.start()
-<<<<<<< HEAD
 vuln2.start(resource_group)
 #vuln4.start(resource_group)
 #vuln5.start(resource_group)
-=======
-
-vuln4.start(resource_group)
-#vuln5.start(resource_group)
->>>>>>> Dev
