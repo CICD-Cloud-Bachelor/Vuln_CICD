dca85b3a4d18304454fc63dd0caf8fcaddbb4a8a
Mathias Mellingen <mellingenmathias@gmail.com>
2024-02-12 14:34:36 +0100

    Formated workitem class

--- source/workitem.py
+++ source/workitem.py
@@ -1,4 +1,5 @@
 from pulumi.dynamic import ResourceProvider, Resource, CreateResult
+import pulumi_azuredevops as azuredevops
 import requests, pulumi, random
 from faker import Faker
 import configparser
@@ -9,34 +10,69 @@ config.read('config.ini')
 USERNAME = config["AZURE"]["USERNAME"]
 PAT = config["AZURE"]["PAT"]
 
-
-
 class HttpPostProvider(ResourceProvider):
-    def create(self, inputs):
-        response = requests.post(inputs['url'], headers=inputs['headers'], json=inputs['json'], auth=tuple(inputs['auth']))
+    def create(
+            self, 
+            inputs: dict
+        ) -> CreateResult:
+
+        response = requests.post(
+            url=inputs['url'], 
+            headers=inputs['headers'], 
+            json=inputs['json'], 
+            auth=tuple(inputs['auth'])
+        )
+
         pulumi.log.info(f"HTTP request response: {response.text}")
+        
         return CreateResult(id_="1", outs={"response": response.text})
 
 class HttpPostResource(Resource):
-    def __init__(self, name, url, headers, json, auth, opts=None):
-        super().__init__(HttpPostProvider(), name, {
-            "url": url,
-            "headers": headers,
-            "json": json,
-            "auth": auth,
-        }, opts)
+    def __init__(
+            self, 
+            name: str, 
+            url: str, 
+            headers: dict, 
+            json: list, 
+            auth: str, 
+            opts=None
+        ) -> None:
+
+        super().__init__(
+            provider=HttpPostProvider(), 
+            name=name, 
+            props={
+                "url": url,
+                "headers": headers,
+                "json": json,
+                "auth": auth,
+            }, 
+            opts=opts
+        )
 
 
 class WorkItem:
     index = 0
-    def __init__(self, organization_name: str, project_name, depends_on):
+    def __init__(
+            self, 
+            organization_name: str, 
+            project_name: str, 
+            depends_on: azuredevops.Project
+        ) -> None:
         self.organization_name = organization_name
         self.project_name = project_name
         self.auth = (USERNAME, PAT)
         self.headers = {"Content-Type": "application/json-patch+json"}
         self.depends_on = depends_on
 
-    def create(self, type, title, description, comment):
+    def create(
+            self, 
+            type: str, 
+            title: str, 
+            description: str, 
+            comment: str
+        ) -> None:
+
         url = f"https://dev.azure.com/{self.organization_name}/{self.project_name}/_apis/wit/workitems/${type}?api-version=5"
         json = [
             {
@@ -60,7 +96,7 @@ class WorkItem:
         ]
         self.index += 1
         HttpPostResource(
-            f"workitem{self.index}",
+            name=f"workitem{self.index}",
             url=url,
             headers=self.headers,
             json=json,
@@ -68,7 +104,11 @@ class WorkItem:
             opts=pulumi.ResourceOptions(depends_on=[self.depends_on])
         )
 
-    def create_random_work_items(self, amount):
+    def create_random_work_items(
+            self, 
+            amount: int
+        ) -> None:
+
         #faker = Faker()
         work_item_type = [
             "Epic", 
@@ -88,8 +128,8 @@ class WorkItem:
         ]
         for i in range(amount):
             self.create(
-                random.choice(work_item_type), 
-                random.choice(work_item_title), 
-                "This is a detailed description of the work item",
-                f"Comment for task {i}"
+                type=random.choice(work_item_type), 
+                title=random.choice(work_item_title), 
+                description="This is a detailed description of the work item",
+                comment=f"Comment for task {i}"
             )
