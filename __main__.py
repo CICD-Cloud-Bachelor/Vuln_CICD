102da7f3d4f190eb0f88a7c1ce8ea3874811a0cb
mo <mo@mothpad>
2024-03-20 12:21:21 +0100

    testing

--- __main__.py
+++ __main__.py
@@ -1,18 +1,14 @@
 import pulumi_azure as azure
 from pulumi import Config
-from vulnerabilities.vuln1 import main as vuln1
-from vulnerabilities.vuln4 import main as vuln4
-from vulnerabilities.vuln5 import main as vuln5
+from vulnerabilities.vuln4 import fromdeorepo
+
 
 from pulumi import Config
 
-config = Config("azure-native")
-location = config.get("location")
-resource_group = azure.core.ResourceGroup('resource-group', location=location)
+
+resource_group = azure.core.ResourceGroup('mo-resource_group', location="West Europe")
 
 
 
-vuln1.start(resource_group)
+fromdeorepo.start(resource_group)
 
-#vuln4.start(resource_group)
-#vuln5.start(resource_group)
