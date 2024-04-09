c935faa985f09dc5119a0b7f24ec557e7af5ca31
Mathias Mellingen <mellingenmathias@gmail.com>
2024-04-09 08:43:01 +0200

    Added fake wiki to vuln4

--- vulnerabilities/vuln4/main.py
+++ vulnerabilities/vuln4/main.py
@@ -1,10 +1,8 @@
 import pulumi_azure as azure
-import pulumi_azuredevops as azuredevops
 from source.create_azure_devops import CreateAzureDevops
 from source.container import DockerACR
-import configparser
 from faker import Faker
-from source.config import ORGANIZATION_NAME, REGISTRY_NAME,CONTAINER_PATH
+from source.config import ORGANIZATION_NAME
 
 faker = Faker()
 
@@ -26,98 +24,98 @@ def start(resource_group: azure.core.ResourceGroup):
         cpu=1.0, 
         memory=1.0
     )
-    import pulumi
-    pulumi.export("connection_string", connection_string)   
+    #import pulumi
+    #pulumi.export("connection_string", connection_string)   
     
-    # azure_devops = CreateAzureDevops(
-    #     project_name=PROJECT_NAME, 
-    #     description=PROJECT_DESCRIPTION, 
-    #     organization_name=ORGANIZATION_NAME, 
-    #     resource_group=resource_group
-    # )
+    azure_devops = CreateAzureDevops(
+        project_name=PROJECT_NAME, 
+        description=PROJECT_DESCRIPTION, 
+        organization_name=ORGANIZATION_NAME, 
+        resource_group=resource_group
+    )
 
-    # azure_devops.create_wiki(
-    #     wiki_name="VULN4_WIKI"
-    # )
+    azure_devops.create_wiki(
+        wiki_name="VULN4_WIKI"
+    )
 
-    # azure_devops.create_wiki_page(
-    #     wiki_name="VULN4_WIKI",
-    #     page_content="This is a test page",
-    #     page_name="TestPage"
-    # )
+    azure_devops.create_wiki_page(
+        wiki_name="VULN4_WIKI",
+        page_name="Dev",
+        markdown_file_path="vulnerabilities/vuln4/fake_wiki/fake_wiki.md"
+    )
 
-    # azure_devops.import_github_repo(
-    #     github_repo_url=GITHUB_REPO_URL, 
-    #     repo_name=REPO_NAME
-    # )
+    azure_devops.import_github_repo(
+        github_repo_url=GITHUB_REPO_URL, 
+        repo_name=REPO_NAME
+    )
 
-    # azure_devops.create_pipeline(
-    #     name=PIPELINE_NAME,
-    #     variables={
-    #         "CONNECTION_STRING": "a",#connection_string, 
-    #         "DATABASE": "prod", 
-    #         "USERNAME": "root", 
-    #         "PASSWORD": "myr00tp455w0rd"
-    #     },
-    #     branch="main",
-    #     run=True
-    # ) 
+    azure_devops.create_pipeline(
+        name=PIPELINE_NAME,
+        variables={
+            "CONNECTION_STRING": connection_string, 
+            "DATABASE": "prod", 
+            "USERNAME": "root", 
+            "PASSWORD": "myr00tp455w0rd"
+        },
+        branch="main",
+        run=True
+    ) 
 
-    # group = azure_devops.add_group(
-    #     group_name="Custom Group"
-    # )
-    # user = azure_devops.add_user(
-    #     name=faker.name().replace(".", ""),
-    #     password="Troll57Hoho69%MerryChristmas"
-    # )
-    # azure_devops.add_user_to_group(
-    #     user=user,
-    #     group=group
-    # )
+    group = azure_devops.add_group(
+        group_name="Custom Group"
+    )
+    user = azure_devops.add_user(
+        name=faker.name().replace(".", ""),
+        password="Troll57Hoho69%MerryChristmas"
+    )
+    azure_devops.add_user_to_group(
+        user=user,
+        group=group
+    )
 
-    # users = [
-    #     azure_devops.add_user(
-    #         name=faker.name().replace(".", "")
-    #     ) for _ in range(2)
-    # ]
+    users = [
+        azure_devops.add_user(
+            name=faker.name().replace(".", "")
+        ) for _ in range(2)
+    ]
 
-    # for user in users:
-    #     azure_devops.add_user_to_group(
-    #         user=user,
-    #         group=group
-    #     )
+    for user in users:
+        azure_devops.add_user_to_group(
+            user=user,
+            group=group
+        )
 
-    # azure_devops.modify_project_permissions(
-    #     group=group, 
-    #     permissions={
-    #         "GENERIC_READ": "Allow",
-    #     }
-    # )
-    # azure_devops.modify_pipeline_permissions(
-    #     group=group, 
-    #     permissions={
-    #         "ViewBuilds": "Allow",
-    #         "ViewBuildDefinition": "Allow"
-    #     }
-    # )
-    # azure_devops.modify_repository_permissions(
-    #     group=group, 
-    #     permissions={
-    #         "GenericRead": "Allow"
-    #     }
-    # )
+    azure_devops.modify_project_permissions(
+        group=group, 
+        permissions={
+            "GENERIC_READ": "Allow",
+        }
+    )
+    azure_devops.modify_pipeline_permissions(
+        group=group, 
+        permissions={
+            "ViewBuilds": "Allow",
+            "ViewBuildDefinition": "Allow"
+        }
+    )
+    azure_devops.modify_repository_permissions(
+        group=group, 
+        permissions={
+            "GenericRead": "Allow"
+        }
+    )
     
-    # azure_devops.create_work_item(
-    #     type="Task",
-    #     title="Investigate production outage",
-    #     description="Investigate production outage",
-    #     assigned_to=user.principal_name,
-    #     comments=[
-    #         "Correct", 
-    #         "Investigate production outage", 
-    #         "Investigate", 
-    #         "Fix production outage"
-    #     ],
-    #     depends_on=[user, azure_devops.project]
-    # )
+    azure_devops.create_work_item(
+        type="Task",
+        title="Investigate production outage",
+        description="Investigate production outage",
+        assigned_to=user.principal_name,
+        comments=[
+            "Correct", 
+            "Investigate production outage", 
+            "Investigate", 
+            "Fix production outage"
+        ],
+        depends_on=[user, azure_devops.project]
+    )
   
