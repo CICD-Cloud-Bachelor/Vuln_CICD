90089f66d779e0c23c4279467126f7986e4698a9
Mathias Mellingen <mellingenmathias@gmail.com>
2024-04-16 12:53:20 +0200

    updated container

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import pulumi_azure as azure
 import pulumi
 from vulnerabilities.vuln3 import main as vuln3
+from vulnerabilities.vuln5 import main as vuln5
 from source.container import CtfdContainer
 from source.config import LOCATION
 
