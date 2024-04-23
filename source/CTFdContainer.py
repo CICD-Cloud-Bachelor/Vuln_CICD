4a1c4174319ef42d8ef12dc86ab2aec48ba0d63f
mo <mo@mothpad>
2024-04-23 23:54:03 +0200

    added some documentation and removed testing file

--- source/CTFdContainer.py
+++ source/CTFdContainer.py
@@ -11,26 +11,41 @@ class CtfdContainer:
     """
     Represents a CTFd container.
 
-    This class provides methods to manage a CTFd container, including initializing the container,
-    running docker-compose commands, and replacing challenge flags and descriptions.
+    This class provides methods to initialize and manage a CTFd container. It allows replacing challenge flags and descriptions in the CTFd export zip file, running the container using docker-compose, and performing other related operations.
 
     Attributes:
-        ctfd_export_path (str): The path to the CTFd export zip file.
-        ctfd_path (str): The path to the CTFd directory.
+        flags_db (dict): The database of challenge flags.
+        challenges_db (dict): The database of challenges.
+        descriptions (dict): A dictionary to store vulnerability descriptions.
+        categories (dict): A dictionary to store vulnerability categories.
+        flags (dict): A dictionary to store vulnerability flags.
+        login_name (str): The login name for the Azure DevOps user.
+        entra_password (str): The password for the Azure DevOps user.
+        ctfd_path (str): The path to the CTFd container directory.
+
+    Methods:
+        __init__(self, username: str, password: str): Initializes a new instance of the CtfdContainer class.
+        __run_docker_compose(self, command: list[str]): Runs a docker-compose command and prints its output.
+        __unzip_file(self, zip_file: str, extraction_dir: str) -> None: Extracts a zip file to the specified directory.
+        __zip_dir(self, dir: str, zip_file: str) -> None: Zips a directory and its contents.
+        __delete_dir(self, dir: str) -> None: Deletes a directory and its contents.
+        __write_json(self, file: str, data: dict) -> None: Writes a dictionary to a JSON file.
+        __get_vuln_descriptions_and_categories_and_flags(self) -> None: Retrieves vulnerability descriptions, categories, and flags for each vulnerability module.
+        __fill_flags_db_and_challenges_db(self) -> None: Fills the flags and challenges databases with the necessary data.
+        __make_files_executable(self, path: list[str]) -> None: Changes the permissions of a list of files.
+        __replace_ctfd_export_flags_and_descriptions(self, ctfd_path: str) -> None: Replaces the challenge flags and descriptions in the CTFd export zip file.
     """
-
-    #####
-    # A heads up - The code assumes the ctfd_export template has the challenges set up with placeholder flags.
-    # This is so that the flags json file has an entry for the respective challenges.
-    # The default ctfd_export.zip has 5 challenges configured, with their id being 1-5 respectively.
-    #####
-
+    
     def __init__(self, username: str, password: str):
         """
         Initializes a new instance of the CtfdContainer class.
 
         This method sets the ctfd_path attribute and replaces challenge flags and descriptions
         in the CTFd export zip file. It then runs the docker-compose command to start the container.
+
+        Args:
+            username (str): The username for the Azure DevOps user.
+            password (str): The password for the Azure DevOps user.
         """
         self.flags_db = {
             "count": 0,
@@ -74,7 +89,6 @@ class CtfdContainer:
             print("Error executing docker compose")
             print(stderr.decode())
 
-        
     def __unzip_file(self, zip_file: str, extraction_dir: str) -> None:
         """
         Extracts a zip file to the specified directory.
@@ -113,7 +127,15 @@ class CtfdContainer:
             json.dump(data, f)
     
     def __get_vuln_descriptions_and_categories_and_flags(self) -> None:
-        
+        """
+        Retrieves vulnerability descriptions, categories, and flags for each vulnerability module.
+
+        This method iterates over the vulnerability modules and imports them dynamically using importlib.
+        It then extracts the necessary information from each module and stores it in the respective dictionaries.
+
+        Returns:
+            None
+        """
         vuln_folders = [folder for folder in os.listdir('vulnerabilities/') if folder.startswith('vuln')]
         number_of_vulns = len(vuln_folders)
 
@@ -128,7 +150,21 @@ class CtfdContainer:
             self.categories[vuln_key] = vuln.CHALLENGE_CATEGORY
             self.flags[vuln_key] = vuln.FLAG
     
-    def __fill_flags_db_and_challenges_db(self) -> None:    
+    def __fill_flags_db_and_challenges_db(self) -> None:
+        """
+        Fills the flags and challenges databases with the necessary data.
+
+        This method retrieves vulnerability descriptions, categories, and flags,
+        and populates the challenges and flags databases accordingly. It assigns
+        unique IDs to each challenge and flag, and sets the necessary attributes
+        such as name, description, category, and content.
+
+        Note: The method assumes that the `descriptions`, `categories`, and `flags`
+        dictionaries have already been populated with the necessary data, and are of equal size.
+
+        Returns:
+            None
+        """
         self.__get_vuln_descriptions_and_categories_and_flags()
 
         #TODO: Check for same amount of challenges, flags and categories
