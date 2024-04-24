6406ef2e866ad55dfefd97095ec7b66728db0b90
Mathias Mellingen <mellingenmathias@gmail.com>
2024-04-24 09:07:04 +0200

    Added check to not run CTFd in preview

--- vulnerabilities/vuln1/main.py
+++ vulnerabilities/vuln1/main.py
@@ -10,9 +10,7 @@ REPO_NAME = "VULN1_REPO"
 PIPELINE_NAME = "BUILD_PIPELINE"
 
 CHALLENGE_DESCRIPTION = """
-Dette er første challenge jippi!!
-Den er veldig morro og du kommer til å like den
-Denne er veldig enkel
+This challenge contains a repository and a pipeline that builds the code in the repository. The pipeline has a secret flag that is used during the build process. Your task is to retrieve the flag.
 """
 CHALLENGE_CATEGORY = "Easy"
 FLAG = "FLAG{you_were_not_supposed_to_find_this}"
@@ -28,24 +26,28 @@ def start(
         resource_group=resource_group
     )
 
-    # azure_devops.import_github_repo(GITHUB_REPO_URL, REPO_NAME)
+    azure_devops.import_github_repo(
+        github_repo_url=GITHUB_REPO_URL, 
+        repo_name=REPO_NAME,
+        is_private=False
+    )
 
-    # azure_devops.create_pipeline(
-    #     name=PIPELINE_NAME,
-    #     variables={
-    #         "FLAG": FLAG
-    #     },
-    #     branch="main",
-    #     run=True
-    # ) 
+    azure_devops.create_pipeline(
+        name=PIPELINE_NAME,
+        variables={
+            "FLAG": FLAG
+        },
+        branch="main",
+        run=True
+    ) 
 
-    # devops_user = CreateAzureDevops.add_entra_user_to_devops(user["entra_user"])
+    devops_user = CreateAzureDevops.add_entra_user_to_devops(user)
     
-    azure_devops.create_wiki(
-        wiki_name="VULN1_WIKI"
-    )
-    azure_devops.create_wiki_page(
-        wiki_name="VULN1_WIKI",
-        page_name="VULN1_CHALLENGE",
-        markdown_file_path="vulnerabilities/vuln1/README.md"
-    )
+    # azure_devops.create_wiki(
+    #     wiki_name="VULN1_WIKI"
+    # )
+    # azure_devops.create_wiki_page(
+    #     wiki_name="VULN1_WIKI",
+    #     page_name="VULN1_CHALLENGE",
+    #     markdown_file_path="vulnerabilities/vuln1/README.md"
+    # )
